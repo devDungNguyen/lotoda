eba751d2a9c44ef95aee24a84a405f5eb95e9e1a
tris <kaynguywn22898@gmail.com>
2024-05-06 15:38:00 +0700

    update menu

--- src/app/utils/definitions.ts
+++ src/app/utils/definitions.ts
@@ -11,4 +11,40 @@ export const ADMIN_SIDENAV: MenuItem[] = [
     label: 'IoT Dashboard',
     icon: 'saxElement3Outline',
   },
+  {
+    path: '/divices-management',
+    label: 'Devices Management',
+    icon: 'saxElement4Outline',
+  },
+  {
+    path: '/realtime-data',
+    label: 'Realtime Data',
+    icon: 'saxDataOutline',
+  },
+  {
+    path: '/alert-rules',
+    label: 'Alert Rules',
+    icon: 'saxNotificationBingOutline',
+  },
+  {
+    path: '/setting',
+    label: 'Setting',
+    icon: 'saxSetting2Outline',
+  },
+  {
+    path: '/history-report',
+    label: 'History & Report',
+    icon: 'saxChart3Outline',
+  },
+  {
+    path: '/profile',
+    label: 'Profile',
+    icon: 'saxProfile2userOutline',
+  },
+  {
+    path: '/support',
+    label: 'Support',
+    icon: 'saxExportOutline',
+  },
 ];
+
