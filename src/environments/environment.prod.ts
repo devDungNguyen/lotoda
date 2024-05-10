8be7a7ad5080ad7f57bb8898157330359ef828bf
devDungNguyen <nguyenmanhdung.dev@gmail.com>
2024-05-10 11:51:11 +0700

    add evironment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://dashboard.lotoda.vn:8443/api',
+  api: {
+    auth: {
+      login: 'login',
+      register: 'register',
+      user: {
+        get: 'users',
+      },
+    },
+  },
 };
