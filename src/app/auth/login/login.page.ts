4429e5b44a2c04bd78a6bed4a9dca599fa030135
tris <kaynguywn22898@gmail.com>
2024-05-10 09:30:41 +0700

    add formdata

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { LoginBody } from 'src/app/utils/interfaces';
 
@@ -8,23 +9,43 @@ import { LoginBody } from 'src/app/utils/interfaces';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  user: any;
+  email: any;
+  password: any;
   formData: LoginBody;
 
-  constructor(private authService: AuthService) {
+
+    // 1
+  constructor(private authService: AuthService, private router: Router) {
     this.formData = {
-      email: 'nguyemanhdung@gmail.com',
-      password: '123456789',
+      email: '',
+      password: '',
     };
   }
 
-  ngOnInit() {
-    return 0;
-  }
+  // email: 'nguyenmanhdung.dev@gmail.com',
+  // password: '123456789',
+
+
+  // login() {
+
+  //   this.formData.email = this.email
+
+  //   console.log('Username:', this.formData.email);
+  // }
 
   login() {
-    this.authService.login(this.formData).subscribe((response) => {
-      this.user = response;
-    });
+    try {
+
+      this.formData.email = this.email;
+      this.formData.password = this.password;
+
+      this.authService.login(this.formData);
+      alert('Login successfully');
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  ngOnInit() {
   }
 }
