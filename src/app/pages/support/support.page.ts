4eacd463c738b85dd09910129b7dd65de4452384
devDungNguyen <nguyenmanhdung.dev@gmail.com>
2024-05-27 14:01:28 +0700

    support content

--- src/app/pages/support/support.page.ts
+++ src/app/pages/support/support.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,96 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./support.page.scss'],
 })
 export class SupportPage implements OnInit {
+  website: string;
+  email: string;
+  image: string;
+  isModalOpen = false;
 
-  constructor() { }
+  contact: {
+    value: string;
+    icon: string;
+  }[];
 
-  ngOnInit() {
+  socials: {
+    value: string;
+    label: string;
+    icon: string;
+  }[];
+
+  steps: {
+    step: number;
+    content: string;
+  }[];
+
+  constructor(private http: HttpClient) {
+    this.website = 'www.lotoda.vn';
+    this.email = 'support@lotoda.vn';
+    this.image = 'https://dashboard.lotoda.vn/assets/images/icon_lotoda.jpg';
+    this.contact = [
+      {
+        value: '+84 286 278 327',
+        icon: 'headset-outline',
+      },
+      {
+        value: '+84 932 647 607',
+        icon: 'phone-portrait-outline',
+      },
+      {
+        value: this.email,
+        icon: 'mail-outline',
+      },
+      {
+        value: this.website,
+        icon: 'link-outline',
+      },
+    ];
+
+    this.socials = [
+      {
+        value: '@lotodavn',
+        label: 'Facebook',
+        icon: 'logo-facebook',
+      },
+      {
+        value: '@lotoda_iot',
+        label: 'Linkedin',
+        icon: 'logo-linkedin',
+      },
+    ];
+
+    this.steps = [
+      {
+        step: 1,
+        content: 'Go to Settings',
+      },
+      {
+        step: 2,
+        content: 'Enter User ID Key and Password ID Key',
+      },
+      {
+        step: 3,
+        content: 'Add some Topics (Channels)',
+      },
+      {
+        step: 4,
+        content: "Click on 'Connect ...' to connect to LOTODA's system",
+      },
+      {
+        step: 5,
+        content: 'Go to Dasboard',
+      },
+      {
+        step: 6,
+        content: 'Add widgets and choose Topic (channel) for widget',
+      },
+    ];
   }
 
+  ngOnInit() {
+    return 0;
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#edf8ff",
+          100: "#d6edff",
+          200: "#b5e0ff",
+          300: "#83cfff",
+          400: "#48b3ff",
+          500: "#1e8eff",
+          600: "#066dff",
+          700: "#0056f7",
+          800: "#0844c5",
+          900: "#0d3e9b",
+          950: "#0e275d",
+        },
+      },
+    },
   },
   plugins: [],
 };
