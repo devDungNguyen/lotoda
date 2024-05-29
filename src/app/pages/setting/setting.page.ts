ecb379d07b9c563032e98fd9dccd62f2d0978830
tris <kaynguywn22898@gmail.com>
2024-05-29 09:00:44 +0700

    fix ui setting page

--- src/app/pages/setting/setting.page.ts
+++ src/app/pages/setting/setting.page.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting.page.scss'],
 })
 export class SettingPage implements OnInit {
+  formInput: {
+    type: string;
+    id: string;
+    label: string;
+    ngModel: string;
+    placeholder: string;
+    icon:string;
+  }[];
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit() {
+    this.formInput = [
+      {
+        type: 'text',
+        id: 'userkey',
+        label: 'User ID Key',
+        ngModel: '',
+        placeholder: 'Enter Username ID Key',
+        icon:'person'
+      },
+      {
+        type: 'password',
+        id: 'passwordkey',
+        label: 'Password ID Key',
+        ngModel: '',
+        placeholder: 'Enter Password ID Key',
+        icon:'key'
+      },
+    ]
   }
 
 }
